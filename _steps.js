d6c20283e8d798c2359b21cc3498851cf3e177d7
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-22 15:28:28 +0600

    added all the steps

---
+++ _steps.js
@@ -0,0 +1,25 @@
+/**
+ * Create a node server with 5 steps
+ * create folder
+ * npm init
+ * npm i express cors mongodb
+ * script-dev: nodemon index.js
+ * use 5 steps to create node server
+ * -----------------------------
+ * 
+ * ====================
+ * CREATE ATLAS MONGODB
+ * ==================== 
+ *
+ * 1. signup
+ * 2. create cluster
+ * 3. create user dbUser and Password
+ * 4. Network access ----> allow ip address
+ * 5. database -> connect -> code copy paste in index.js
+ * 
+ * ==========================
+ * CRUD OPERATION
+ * ==========================
+ * 1. node mongodb CRUD Fundamentals
+ * 2. create async await run function
+ */
