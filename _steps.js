99615bda7458e8e52c0d6269f61434c0c6a70c1e
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-22 17:00:40 +0600

    create a crud server

--- _steps.js
+++ _steps.js
@@ -39,4 +39,6 @@
  * 3. collection.find(query)
  * 4. cursor.toArray()
  * 5. return the result
+ * 6. fetch client useEffect and display like I have done before
+ * -----------------------------
  */
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,47 +10,80 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Running my node CRUD server');
-})
 
-//mongodb user info
-// User: dbuser1
-// Password: LlGf6EfsbWgL6qsL
-
+// user:dbuser1
+// password:xLeDtsSERnlIPrJ3
 
 
 const uri = "mongodb+srv://dbuser1:LlGf6EfsbWgL6qsL@cluster0.ntqok.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run() {
-    try {
+async function run(){
+    try{
         await client.connect();
-        const userCollection = client.db("foodExpress").collection("user");
+        const userCollection = client.db('foodExpress').collection('user');
 
-        app.get('/users', async (req,res)=>{
+        // get users
+        app.get('/user', async(req, res) =>{
             const query = {};
             const cursor = userCollection.find(query);
             const users = await cursor.toArray();
             res.send(users);
-        })
-        
-        // POST user: add a new user
-        app.post('/user', async (req, res) => {
+        });
+
+        app.get('/user/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await userCollection.findOne(query);
+            res.send(result);
+        });
+
+        // POST User : add a new user
+        app.post('/user', async(req, res) =>{
             const newUser = req.body;
             console.log('adding new user', newUser);
             const result = await userCollection.insertOne(newUser);
-            res.send({ result: 'success' });
+            res.send(result)
+        });
+
+        // update user
+        app.put('/user/:id', async(req, res) =>{
+            const id = req.params.id;
+            const updatedUser = req.body;
+            const filter = {_id: ObjectId(id)};
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    name: updatedUser.name,
+                    email: updatedUser.email
+                }
+            };
+            const result = await userCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+
         })
+
+        // delete a user
+        app.delete('/user/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
-    finally {
-        // await client.close();
+    finally{
+
     }
 }
 
-
 run().catch(console.dir);
 
-app.listen(port, () => {
-    console.log("CRUD my server is running")
+
+app.get('/', (req, res) =>{
+    res.send('Running My Node CRUD Server');
+});
+
+app.listen(port, () =>{
+    console.log('CRUD Server is running');
 })
