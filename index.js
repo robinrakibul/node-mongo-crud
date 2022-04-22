138e9e2d9ed606cf1a5fdfe4ada458e93a8e579f
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-22 15:04:36 +0600

    added mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,6 +11,22 @@ app.use(express.json());
 app.get('/',(req,res)=>{
     res.send('Running my node CRUD server');
 })
+
+//mongodb user info
+// User: dbuser1
+// Password: LlGf6EfsbWgL6qsL
+
+
+
+const uri = "mongodb+srv://dbuser1:LlGf6EfsbWgL6qsL@cluster0.ntqok.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('db connected')
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.listen(port,()=>{
     console.log("CRUD my server is running")
